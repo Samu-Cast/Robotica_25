aa45bf56b20393deb9e71e278a945bc3f9b2c212
Salemme02 <samueleverna2@gmail.com>
2026-02-11 12:41:32 +0100

    ristruttura per il fisico

--- src/sense/entrypoint.sh
+++ src/sense/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-# Carica l'ambiente ROS 2 base
-source /opt/ros/jazzy/setup.bash
+# Carica l'ambiente ROS 2 Foxy
+source /opt/ros/foxy/setup.bash
 
-# Carica il tuo workspace (se compilato)
+# Carica il workspace irobot_create_msgs
+if [ -f /home/ubuntu/irobot_ws/install/setup.bash ]; then
+  source /home/ubuntu/irobot_ws/install/setup.bash
+fi
+
+# Carica il workspace del modulo (se compilato)
 if [ -f /home/ubuntu/sense_ws/install/setup.bash ]; then
   source /home/ubuntu/sense_ws/install/setup.bash
 fi
